76d068471b6d772caee6f3e9ff7f8b7d6fbaa487
Thomas Martinez <thomasmartinez.student@careerdevs.com>
2019-04-29 16:51:15 +0000

    updated serverfile

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const fruits = require('./data');
+const newWords = require('./spellcheck');
+const app = express();
+const port = 8080; // To grab default port, 5000 is for locally running ---> port = process.env.PORT || 5000;
+
+// console.log(fruits);
+console.log(newWords);
+
+app.get('/', function(req, res){
+    const userName = req.query.name
+    res.send(`Hello ${userName}!`);
+});
+
+app.listen(port, () => console.log(`App listening on port ${port}!`));
