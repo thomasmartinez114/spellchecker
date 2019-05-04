5a4fc8c475094b258ed5ad0d7864390c3f52efa5
Thomas Martinez <thomasmartinez.student@careerdevs.com>
2019-05-04 14:50:44 +0000

    updated dictionary.js

---
+++ main/dictionary.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+
+// Line 4 is for testing a few words
+let dictionary = fs.readFileSync('words_test.json');
+
+// this is the main dictionary source
+// let dictionary = fs.readFileSync('words_dictionary.json');
+
+let mainDictionary = JSON.parse(dictionary);
+
+// console.log(dictionary);
+// console.log(mainDictionary);
+
+module.exports = dictionary;
