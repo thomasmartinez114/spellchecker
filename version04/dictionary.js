7092cb67dc2a41c8b2efa145d881dc3246d09a91
Thomas Martinez <thomasmartinez.student@careerdevs.com>
2019-05-05 22:32:59 +0000

    added public/ with index styles and script // added dictionary.js

---
+++ version04/dictionary.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+
+let ogDictionary = fs.readFileSync("words_test.json");
+//let ogDictionary = fs.readFileSync("words_dictionary.json");
+
+let dictionary = JSON.parse(ogDictionary);
+
+module.exports = dictionary;
--- version04/server.js
+++ version04/server.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const app = express();
+const dictionary = require('./dictionary.js');
 const port = process.env.PORT || 5000;
 
+// static page
+app.use(express.static('public'));
 
+app.get('/dictionary', (req, res) => {
+    const
+})
 
 app.listen(port, () => console.log(`Server listening on ${port}`));
