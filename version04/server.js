0b55a694a42794d6dd9b126741cb800758869c99
Thomas Martinez <thomasmartinez.student@careerdevs.com>
2019-05-06 18:40:16 +0000

    pushed index

--- version04/server.js
+++ version04/server.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const app = express();
 const dictionary = require('./dictionary.js');
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 
 // static page
 app.use(express.static('public'));
 
 app.get('/dictionary', (req, res) => {
-    const
+    const word = req.query.word;
+        res.send(`The word ${word} is in the dictionary! Congrats!`);
 })
 
 app.listen(port, () => console.log(`Server listening on ${port}`));
